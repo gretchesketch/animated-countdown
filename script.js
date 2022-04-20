7d130b44050e3fd02f520af7218aed8d6fd3087a
gretchesketch <gretchenjensen92@gmail.com>
2022-04-20 17:21:37 -0400

    added bouncing affect to numbers as they rotate and started on the javscript with the html element hooks

---
+++ script.js
@@ -0,0 +1,9 @@
+// using query select all is how i will bring in all the spans inside of nums
+const nums = document.querySelectorAll(".nums span")
+const counter = document.querySelector(".counter")
+const finalMessage = document.querySelector(".final")
+const replay = document.querySelector("#replay")
+
+runAnimation()
+
+function runAnimation()
