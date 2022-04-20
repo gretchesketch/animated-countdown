5d2024f7cd28dbce28d72c0c79e8f76000e80934
gretchesketch <gretchenjensen92@gmail.com>
2022-04-20 17:37:04 -0400

    started javascript code building runanimation function

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-// using query select all is how i will bring in all the spans inside of nums
+// using query select all is how i will bring in all the spans inside of nums.
+// query selecter all is creating a node list.
 const nums = document.querySelectorAll(".nums span")
 const counter = document.querySelector(".counter")
 const finalMessage = document.querySelector(".final")
 const replay = document.querySelector("#replay")
 
+// calling animation function.
 runAnimation()
 
-function runAnimation()
+ // creating animation function.
+function runAnimation() {
+    //loop through the numbers we brought in under the name of nums.
+    // using a call back in the for each i am calling mine num to represent each individual digit under the name nums(plural).
+    // giving it a second parameter of index which will treat it like an array that starts at 0.
+    nums.forEach((num, index) => {
+        //creating a variable to get the next to last digit in the array.
+        const nextToLast = nums.length - 1
+
+        // creating an event lister because we want to know when the event ends.
+        //using num to represent the current number the event is effecting.
+        // animationend is just lik click. its a built in event.
+        num.addEventListener("animationend", (e) => {
+
+        })
+    })
+}
